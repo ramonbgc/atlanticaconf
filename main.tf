90c7e15554e718b08a14f26a2905def2b53c2ad7
ramon gonzalez <ramongonzalezcastro@gmail.com>
2019-10-03 09:09:19 +0200

    checking creation

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 # Configure the provider to reach Azure
 provider "azurerm" {
     version = "=1.34.0"
+
+  ### if using tfvars and not the Terraform Cloud
+  # subscription_id = "${var.subscription_id}"
+  # client_id = "${var.client_id}"
+  # client_secret = "${var.client_secret}"
+  # tenant_id = "${var.tenant_id}"
 }
 
 terraform {
@@ -11,4 +17,14 @@ terraform {
       name = "atlanticaconf"
     }
   }
+
+  ### if using azure storage account and not the Terraform Cloud as backend
+  #  backend "azurerm" {
+  #    resource_group_name   = "generaluse-rg"
+  #    storage_account_name  = "rgcgeneralusesa"
+  #    container_name        = "tfstate"
+  #    key                   = "terraform.tfstate"
+  #  }
+
+
 }
--- test.tf
+++ test.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
         name = "testResourceGroup"
-        location = "westeu"
+        location = "westeurope"
 }
