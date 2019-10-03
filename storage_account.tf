460615b15211667219cf45483df9d107e796618c
ramon gonzalez <ramongonzalezcastro@gmail.com>
2019-10-03 11:15:19 +0200

    creating a storage container

--- storage_account.tf
+++ storage_account.tf
@@ -7,9 +7,15 @@ resource "azurerm_storage_account" "sac" {
   account_tier              = "Standard"
   access_tier               = "Hot"
   account_replication_type  = "LRS"
-  
+
   is_hns_enabled            = true
 
   tags                      = "${var.tags}"
 }
 
+resource "azurerm_storage_container" "sc" {
+  name                  = "destination"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  storage_account_name  = "${azurerm_storage_account.sac.name}"
+  container_access_type = "private"
+}
