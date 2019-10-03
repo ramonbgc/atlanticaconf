c5bb6b450cd585c7f0b00768fc9f2c4aa9ade3ae
ramon gonzalez <ramongonzalezcastro@gmail.com>
2019-10-03 12:27:43 +0200

    storage v2

--- storage_account.tf
+++ storage_account.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "sac" {
   resource_group_name       = "${azurerm_resource_group.rg.name}"
   location                  = "${azurerm_resource_group.rg.location}"
 
-  account_kind              = "BlobStorage"
+  account_kind              = "StorageV2"
   account_tier              = "Standard"
   access_tier               = "Hot"
   account_replication_type  = "LRS"
